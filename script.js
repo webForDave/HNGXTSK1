ac30466653cf370e3f6ec49758f1f000aee3bceb
David <olalekaneniola203@gmail.com>
2023-09-08 11:55:33 -0700

    Tried different code for day and time.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-let currentDate = new Date();
-let dayOfWeek = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
-let utcTime = currentDate.toISOString().split('T')[1].substring(0, 8);
+// let currentDate = new Date();
+// let dayOfWeek = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
+// let utcTime = currentDate.toISOString().split('T')[1].substring(0, 8);
 
 
-document.getElementById('current-day').innerHTML = dayOfWeek;
-document.getElementById('utc-time').innerHTML = utcTime;
+// document.getElementById('current-day').innerHTML = dayOfWeek;
+// document.getElementById('utc-time').innerHTML = utcTime;
+
+let dateInstance = new Date();
+let currentDay = dateInstance.getDay();
+let hours = dateInstance.getUTCHours();
+let mins = dateInstance.getUTCMinutes();
+let secs = dateInstance.getUTCSeconds();
+
+document.getElementById('current-day').innerHTML = dateInstance.toLocaleString('default', {weekday: 'long'});
+document.getElementById('utc-time').innerHTML = `${hours} : ${mins} : ${secs}`;
